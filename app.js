a86991e61718294c08abe7cf738528ec7f4b1eaa
Derrick Foster <derrick.j.foster@gmail.com>
2019-04-28 12:04:56 -0600

    Css looking good, working on Jquery and AJAX

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 console.log($);
+
+const $container = $('#container')
+const makeBoxes = () =>{
+  for (var i = 0; i < 5; i++) {
+    const $characterBoxes = $('<div>')
+    console.log(i);
+    $('container').append($characterBoxes[i])
+
+  }
+}
+
+
+$(()=>{
+  makeBoxes();
+})
