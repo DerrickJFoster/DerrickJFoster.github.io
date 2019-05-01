8a0a05104666083eecf8d1a843c87c84d44bf49e
Derrick Foster <derrick.j.foster@gmail.com>
2019-05-01 16:31:11 -0600

    Test

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 console.log($);
 
-const baseURL = `https://gateway.marvel.com:443/v1/public/Character?`
-let ts = `ts=1234567890`
-const publicKey = `apiKey=b838fe84e1736b52702b1c6cc30a90ed`;
+const baseURL = `https://gateway.marvel.com:443/v1/public/characters?`
+let ts = `ts=1`
+const publicKey = `apikey=b838fe84e1736b52702b1c6cc30a90ed`;
 const privateKey = `94bb7a78e50a9bb81db19ce1b16b9f74fabe45e9`;
 
-let hash = `hash=df2259107bb37d67ace527b65c3d17c9`
+let hash = `hash=e8eb73afeeed73e477a5c76650af0e4a`
 let queryType = `nameStartsWith=`
 
 let apiURL = baseURL + ts + '&' + publicKey + '&' + hash
