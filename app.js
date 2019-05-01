b19f16571edf1dd76be57df52e72313d0a85b155
Derrick Foster <derrick.j.foster@gmail.com>
2019-04-30 20:01:24 -0600

    trying to figure out api hash - still broken

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 console.log($);
 
+const baseURL = `https://gateway.marvel.com:443/v1/public/Character?`
+let ts = `ts=1234567890`
+const publicKey = `apiKey=b838fe84e1736b52702b1c6cc30a90ed`;
+const privateKey = `94bb7a78e50a9bb81db19ce1b16b9f74fabe45e9`;
+
+let hash = `hash=df2259107bb37d67ace527b65c3d17c9`
+
+
+let apiURL = baseURL + ts + '&' + publicKey + '&' + hash
+
+//hash must = md5 of ts= + private + public
+
 const $container = $('#container')
 const makeBoxes = () =>{
   for (var i = 0; i <= 100; i++) {
     const $characterBoxes = $('<div>').addClass('boxes').appendTo('div.container')
     console.log($characterBoxes[i]);
+
   }
 }
 // test appendTo
@@ -15,4 +28,5 @@ const makeBoxes = () =>{
 $(()=>{
   makeBoxes();
   // test();
+console.log(apiURL);
 })
