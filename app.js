2e2fa74bf19782079d0f624c88349917c046a641
Derrick Foster <derrick.j.foster@gmail.com>
2019-05-01 22:41:00 -0600

    added some css effects to character icons

--- app.js
+++ app.js
@@ -12,54 +12,32 @@ const apiURL = baseURL + ts + '&' + publicKey+ '&' + queryType + '&' + hash
 
 //hash must = md5 of ts= + private + public
 
-// const getInfo = ()=>{
-//   $.ajax({
-//     url: apiURL
-//   }).then((data) =>{
-//     $('div.boxes').html(`
-//       <img src="${data.data.results[0].thumbnail.path}/portrait_small.${data.data.results[0].thumbnail.extension}" alt="spidey">
-//       <h6>${data.data.results[0].name}
-//     `)
-//   }, (error) => {
-//     console.log(error);
-//   })
-// }
-  // let queryType = `limit= ${document.getElementById('text').value}`
-  // apiURL();
 
 const getInfo = () =>{
   $.ajax({
     url: apiURL
   }).then((data) =>{
-      for (var i = 0; i < 10; i++) {
+      for (var i = 0; i < 100; i++) {
         console.log(data.data.results[i].name);
         makeBoxes(data.data.results[i])
-
-
   }
 })
 }
 
-// $('div.boxes').html(`
-// <img src="${data.data.results[i].thumbnail.path}/portrait_small.${data.data.results[i].thumbnail.extension}" alt="">
-// <h6>${data.data.results[i].name}</h6>
-// `)
 
 const $container = $('#container')
 
 const makeBoxes = (character) =>{
-  console.log(character);
-
- $('<div>').addClass('boxes').html(`
- <img src="${character.thumbnail.path}/portrait_small.${character.thumbnail.extension}" alt="">
- <h6>${character.name}</h6>
- `).appendTo('div.container')
+    console.log(character);
+   $('<div>').addClass('boxes').html(`
+   <img src="${character.thumbnail.path}/portrait_medium.${character.thumbnail.extension}" alt="">
+   <div class='name'>${character.name}</div>
+   `).appendTo('div.container')
 }
 
 
 
 
 $(()=>{
-  // makeBoxes();
   getInfo();
 })
