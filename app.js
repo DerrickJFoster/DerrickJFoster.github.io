47ca8bfd944757be1101e437909c0fafbeca17b8
Derrick Foster <derrick.j.foster@gmail.com>
2019-05-01 20:56:15 -0600

    character boxes now populate with individual character picture and name

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const publicKey = `apikey=b838fe84e1736b52702b1c6cc30a90ed`;
 const privateKey = `94bb7a78e50a9bb81db19ce1b16b9f74fabe45e9`;
 
 let hash = `hash=e8eb73afeeed73e477a5c76650af0e4a`
-let queryType = `limit=10`
+let queryType = `limit=100`
 
 const apiURL = baseURL + ts + '&' + publicKey+ '&' + queryType + '&' + hash
 
@@ -27,32 +27,39 @@ const apiURL = baseURL + ts + '&' + publicKey+ '&' + queryType + '&' + hash
   // let queryType = `limit= ${document.getElementById('text').value}`
   // apiURL();
 
-const getInfo = (i) =>{
+const getInfo = () =>{
   $.ajax({
     url: apiURL
   }).then((data) =>{
-    $('div.boxes').html(`
-      <img src="${data.data.results[i].thumbnail.path}/portrait_small.${data.data.results[i].thumbnail.extension}" alt="spidey">
-      <h6>${data.data.results[i].name}</h6>
-    `).then(console.log(data.data.results[i]))
-  }, (error) => {
-    console.log(error);
-  })
+      for (var i = 0; i < 10; i++) {
+        console.log(data.data.results[i].name);
+        makeBoxes(data.data.results[i])
+
+
+  }
+})
 }
+
+// $('div.boxes').html(`
+// <img src="${data.data.results[i].thumbnail.path}/portrait_small.${data.data.results[i].thumbnail.extension}" alt="">
+// <h6>${data.data.results[i].name}</h6>
+// `)
 
 const $container = $('#container')
 
-const makeBoxes = () =>{
-  for (var i = 0; i < 10; i++) {
-    const $characterBoxes = $('<div>').addClass('boxes').appendTo('div.container')
-    console.log($characterBoxes[i]);
-    getInfo(i);
-    }
-  }
+const makeBoxes = (character) =>{
+  console.log(character);
+
+ $('<div>').addClass('boxes').html(`
+ <img src="${character.thumbnail.path}/portrait_small.${character.thumbnail.extension}" alt="">
+ <h6>${character.name}</h6>
+ `).appendTo('div.container')
+}
+
 
 
 
 $(()=>{
-  makeBoxes();
-  // getInfo();
+  // makeBoxes();
+  getInfo();
 })
